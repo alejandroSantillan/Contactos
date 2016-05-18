a4f33abdd7a18116597c78aeb029a5248fec213c
Alejandro <alejandro.santillan@globalquark.com.mx>
2016-05-18 16:43:34 -0500

    Primer commit

---
+++ app/src/main/java/alejandro/com/contactos/ConfirmacionActivity.java
@@ -0,0 +1,68 @@
+package alejandro.com.contactos;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
+import android.view.View;
+import android.widget.TextView;
+
+public class ConfirmacionActivity extends AppCompatActivity {
+
+    private TextView nombre;
+    private TextView fecha;
+    private TextView telefono;
+    private TextView email;
+    private TextView descripcion;
+
+    private AppCompatButton boton;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirmacion);
+
+        nombre = (TextView) findViewById(R.id.nombre);
+        fecha = (TextView) findViewById(R.id.fecha);
+        telefono = (TextView) findViewById(R.id.telefono);
+        email = (TextView) findViewById(R.id.email);
+        descripcion = (TextView) findViewById(R.id.descripcion);
+
+        boton = (AppCompatButton) findViewById(R.id.editar_boton);
+
+        Bundle extras = getIntent().getExtras();
+
+        nombre.setText(extras.getString(Contacto.NOMBRE));
+        fecha.setText(extras.getString(Contacto.FECHA));
+        telefono.setText(extras.getString(Contacto.TELEFONO));
+        email.setText(extras.getString(Contacto.EMAIL));
+        descripcion.setText(extras.getString(Contacto.DESCRIPCION));
+
+
+        editarDatos();
+
+    }
+
+    private void editarDatos() {
+
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ConfirmacionActivity.this,MainActivity.class);
+
+                intent.putExtra(Contacto.NOMBRE,nombre.getText().toString());
+                intent.putExtra(Contacto.FECHA,fecha.getText().toString());
+                intent.putExtra(Contacto.TELEFONO,telefono.getText().toString());
+                intent.putExtra(Contacto.EMAIL,email.getText().toString());
+                intent.putExtra(Contacto.DESCRIPCION,descripcion.getText().toString());
+
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/alejandro/com/contactos/MainActivity.java
@@ -0,0 +1,103 @@
+package alejandro.com.contactos;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private static final String TAG = MainActivity.class.getName();
+
+
+    private TextInputEditText nombre;
+    private TextInputEditText fecha;
+    private TextInputEditText telefono;
+    private TextInputEditText correo;
+    private TextInputEditText descripcion;
+
+    private AppCompatButton boton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nombre = (TextInputEditText) findViewById(R.id.nombre);
+        fecha = (TextInputEditText) findViewById(R.id.fecha);
+        correo = (TextInputEditText) findViewById(R.id.email);
+        telefono = (TextInputEditText) findViewById(R.id.telefono);
+        descripcion = (TextInputEditText) findViewById(R.id.descripcion);
+        boton = (AppCompatButton) findViewById(R.id.boton_confirmar);
+
+
+
+        if (getIntent().hasExtra(Contacto.NOMBRE)) {
+            Bundle extras = getIntent().getExtras();
+            nombre.setText(extras.getString(Contacto.NOMBRE));
+            fecha.setText(extras.getString(Contacto.FECHA));
+            telefono.setText(extras.getString(Contacto.TELEFONO));
+            correo.setText(extras.getString(Contacto.EMAIL));
+            descripcion.setText(extras.getString(Contacto.DESCRIPCION));
+        }
+
+        datePickerDialog();
+        confirmarDatos();
+
+    }
+
+    private void confirmarDatos() {
+
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, ConfirmacionActivity.class);
+
+                intent.putExtra(Contacto.NOMBRE, nombre.getText().toString());
+                intent.putExtra(Contacto.FECHA, fecha.getText().toString());
+                intent.putExtra(Contacto.TELEFONO, telefono.getText().toString());
+                intent.putExtra(Contacto.EMAIL, correo.getText().toString());
+                intent.putExtra(Contacto.DESCRIPCION, descripcion.getText().toString());
+
+                startActivity(intent);
+                finish();
+            }
+        });
+
+    }
+
+    private void datePickerDialog() {
+
+        Log.i(TAG, "creando date picker");
+        fecha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                DatePickerFragment fragment = new DatePickerFragment();
+                fragment.setFecha(fecha);
+
+                fragment.show(getSupportFragmentManager(), "datePicker");
+            }
+        });
+
+        fecha.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    DatePickerFragment fragment = new DatePickerFragment();
+                    fragment.setFecha(fecha);
+                    fragment.show(getSupportFragmentManager(), "datePicker");
+                }
+            }
+        });
+
+    }
+
+
+}
